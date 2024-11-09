e60f879d1c45d809326c98c882e81752dc280be2
Daeuni <ekdmsjj@gmail.com>
2024-11-09 23:50:04 +0900

    about페이지 끝

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,12 +1,37 @@
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const BackBtn = styled.button`
+  background-color:white;
+  border:1px solid black;
+  width:80px;
+  height:30px;
+  cursor:pointer;
+  border-radius:12px;
+  &:hover{
+    border:1px solid green;
+  }
+`
+const Abt = styled.div`
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+  margin-top:300px;
+`
+
 function About(): JSX.Element {
+  const navigate = useNavigate();
+  const gotoMain = ()=>navigate('/');
   return (
-    <div>
-      <div style={{marginTop:'300px',display: 'flex', justifyContent: 'center', alignItems:'center',flexDirection:'column'}}>
+    <>
+      <Abt>
         This website based on
         <a href="https://www.jejuolle.org/trail#/">  제주 올레 트레일</a>
-        <h3>위 사이트에서 더 많은 루트를 구경하세요</h3>
-      </div>
-    </div>
+        <h3>위 사이트에서 더 많은 루트를 살펴보세요</h3>
+        <BackBtn onClick={gotoMain}>첫 페이지로</BackBtn>
+      </Abt>
+    </>
   )
 }
 export default About;
